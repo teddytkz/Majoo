b2ba88e0c3d4c7d7ca3c9a969bc02ef0d10538b2
Teddy <tkzteddy@gmail.com>
2022-05-29 21:00:45 +0700

    Add : Logout Button

--- application/views/dashboard/partial/header.php
+++ application/views/dashboard/partial/header.php
@@ -52,10 +52,9 @@
 					</li>
 
 				</ul>
-				<!-- <form class="d-flex" role="search">
-					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success" type="submit">Search</button>
-				</form> -->
+				<div class="d-flex">
+					<a class="nav-link" href="<?php echo base_url()?>auth/logout">Logout</a>
+				</div>
 			</div>
 		</div>
 	</nav>
