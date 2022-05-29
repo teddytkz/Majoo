0c1b0b1f522494defd138c67510860ef7f816b8f
Teddy <tkzteddy@gmail.com>
2022-05-29 20:32:12 +0700

    Add Sweet Alert and Select 2 depedencies

--- application/views/dashboard/partial/header.php
+++ application/views/dashboard/partial/header.php
@@ -12,6 +12,14 @@
 		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+
+	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
 </head>
 
 <body>
